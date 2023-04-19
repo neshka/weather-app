77e30bc6007e9c25741240b85c0f18bd1ffc52df
neshka <agacode0@gmail.com>
2023-04-19 22:57:56 +0100

    add weather result tiles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import Search from './components/Icons/Search';
+import Weather from './components/Weather';
+import Search from './components/Search';
 import useForecast from './hooks/useForecast';
 
 const App = () => {
-  const {
-    term,
-    options,
-    city,
-    forecast,
-    onInputChange,
-    onSubmit,
-    onOptionSelect
-  } = useForecast();
+ const { term, options, forecast, onInputChange, onSubmit, onOptionSelect } =
+  useForecast();
 
-  return (
-    <main className="flex justify-center items-center bg-gradient-to-br from-yellow-400 via-emerald-400 to-blue-900 h-[100vh] w-full">
-      {forecast ? ('we have forecast') : (
-        <Search term={term} options={options} onInputChange={onInputChange} onOptionSelect={onOptionSelect} onSubmit={onSubmit} />
-      )}
-    </main>
-  );
+ return (
+  <main className="flex justify-center items-center bg-gradient-to-br from-yellow-400 via-emerald-400 to-blue-900 h-[100vh] w-full">
+   {forecast ? (
+    <Weather data={forecast} />
+   ) : (
+    <Search
+     term={term}
+     options={options}
+     onInputChange={onInputChange}
+     onOptionSelect={onOptionSelect}
+     onSubmit={onSubmit}
+    />
+   )}
+  </main>
+ );
 };
 
 export default App;
--- src/hooks/useForecast.ts
+++ src/hooks/useForecast.ts
@@ -1,6 +1,8 @@
 import { useEffect, useState, ChangeEvent } from "react";
 import { Option, Forecast } from "../types";
 
+const BASE_URL = 'https://api.openweathermap.org'
+
 const useForecast = () => {
   const [term, setTerm] = useState('');
   const [options, setOptions] = useState<[]>([]);
@@ -9,12 +11,13 @@ const useForecast = () => {
 
   const getSearchResult = (value: string) => {
     fetch(
-      `https://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=5&appid=${
+      `${BASE_URL}/geo/1.0/direct?q=${value}&limit=5&appid=${
         process.env.REACT_APP_API_KEY
       }`
     )
       .then(res => res.json())
-      .then(data => setOptions(data));
+      .then(data => setOptions(data))
+      .catch((e) => console.log({ e }));
   };
 
   const onInputChange = (
@@ -22,19 +25,23 @@ const useForecast = () => {
   ) => {
     const value = event.target.value.trim();
     setTerm(value);
-    if (term === '') return;
-
-    getSearchResult(value);
+    if (value !== '') {
+        getSearchResult(value)
+    }
   };
 
   const getForecast = (option: Option) => {
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${option.lat}&lon=${option.lon}&units=metric&appid=${
+      `${BASE_URL}/data/2.5/forecast?lat=${option.lat}&lon=${option.lon}&units=metric&lang=en&appid=${
         process.env.REACT_APP_API_KEY
       }`
     )
       .then(res => res.json())
-      .then(data => setForecast(data));
+      .then(data => {
+        const forecastData = { ...data.city, list: data.list.slice(0,16), } as Forecast;
+        setForecast(forecastData);
+    })
+    .catch((e) => console.log({ e }));
   }
 
   const onSubmit = () => {
@@ -56,7 +63,6 @@ const useForecast = () => {
   return {
     term,
     options,
-    city,
     forecast,
     onInputChange,
     onSubmit,
